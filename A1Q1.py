bb5aae9c3d5ad8b59b128f133f251284574e5571
acelaria <arushiace17@gmail.com>
2025-01-28 20:39:19 +0530

    Q1

---
+++ A1Q1.py
@@ -0,0 +1,14 @@
+list_int = []
+for i in range(50):
+    list_int.append(i)
+print(list_int)
+
+list_squares = []
+for i in range(1, 51):
+    list_squares.append(i**2)
+print(list_squares)
+
+list_letter = []
+for i in range(1, 27):
+    list_letter.append(chr(96 + i) * i)
+print(list_letter)
