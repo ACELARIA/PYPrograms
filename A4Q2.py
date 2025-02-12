7f07d83262007e72609d41a6a370a3aa3363de76
acelaria <arushiace17@gmail.com>
2025-02-12 22:21:56 +0530

    A4Q2

--- A4Q2.py
+++ A4Q2.py
@@ -1,14 +1,14 @@
 import math
 
-def count_square_integers(A, B):
-    l = math.ceil(math.sqrt(A))
-    r = math.floor(math.sqrt(B))
+def count_square_integers(a,b):
+    l = math.ceil(math.sqrt(a))
+    r = math.floor(math.sqrt(b))
     
-    return max(0, r - l + 1)
+    return max(0,r-l+1)
 
 print("Enter numbers:")
 T = int(input())
 
 for _ in range(T):
-    A, B = map(int, input().split()) 
-    print(count_square_integers(A, B))
+    a,b = map(int, input().split()) 
+    print(count_square_integers(a,b))
